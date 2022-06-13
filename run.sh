f95604e9a2a323626785b0a1f6fb37035b100afc
lizhao <lukehatton685@gmail.com>
2022-06-13 19:45:06 +0800

    run.sh更新
    添加arm设备运行命令

--- run.sh
+++ run.sh
@@ -4,10 +4,16 @@
 #####################################################
 # amd64设备
 # ElasticSearch
-docker run --name skywalking-es --network=bridge-skywalking -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -e ES_JAVA_OPTS="-Xms128m -Xmx512m" -d elasticsearch:7.6.2
+docker run --name skywalking-es --network=bridge-skywalking -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -e ES_JAVA_OPTS="-Xms128m -Xmx512m" -e TZ=Asia/Shanghai -d elasticsearch:7.6.2
 # SkyWalking
-docker run --name skywalking-server --network=bridge-skywalking -p 1234:1234 -p 11800:11800 -p 12800:12800 -e SW_STORAGE=elasticsearch7 -e SW_STORAGE_ES_CLUSTER_NODES=skywalking-es:9200 -d apache/skywalking-oap-server:8.3.0-es7
-# UI
-docker run --name skywalking-ui --network=bridge-skywalking -p 8080:8080 -e SW_OAP_ADDRESS=skywalking-server:12800 -d apache/skywalking-ui:8.3.0
+docker run --name skywalking-server --network=bridge-skywalking -p 1234:1234 -p 11800:11800 -p 12800:12800 -e SW_STORAGE=elasticsearch7 -e SW_STORAGE_ES_CLUSTER_NODES=skywalking-es:9200 -e TZ=Asia/Shanghai -d apache/skywalking-oap-server:8.3.0-es7
+# UI – oap address必须指定使用http协议，否则会出现前端错误
+docker run --name skywalking-ui --network=bridge-skywalking -p 8080:8080 -e SW_OAP_ADDRESS=http://skywalking-server:12800 -e TZ=Asia/Shanghai -d apache/skywalking-ui:8.3.0
 
 # arm设备
+# ElasticSearch
+docker run --name skywalking-es --network=bridge-skywalking -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -e ES_JAVA_OPTS="-Xms128m -Xmx512m" -e TZ=Asia/Shanghai -d elasticsearch:7.14.2
+# SkyWalking
+docker run --name skywalking-server --network=bridge-skywalking -p 1234:1234 -p 11800:11800 -p 12800:12800 -e SW_STORAGE=elasticsearch -e SW_STORAGE_ES_CLUSTER_NODES=skywalking-es:9200 -e TZ=Asia/Shanghai -d apache/skywalking-oap-server:latest
+# UI – oap address必须指定使用http协议，否则会出现前端错误
+docker run --name skywalking-ui --network=bridge-skywalking -p 8080:8080 -e SW_OAP_ADDRESS=http://skywalking-server:12800 -e TZ=Asia/Shanghai -d apache/skywalking-ui:latest
