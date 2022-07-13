3bad578deccff970bd3e31c81cbd600f7ee47f2a
lizhao <lukehatton685@gmail.com>
2022-07-13 21:19:30 +0800

    解决依赖冲突

--- taxi-common/src/main/java/com/example/config/FeignLoggerConfig.java
+++ taxi-common/src/main/java/com/example/config/FeignLoggerConfig.java
@@ -8,6 +8,11 @@ import org.springframework.context.annotation.Configuration;
 /**
  * project : taxi-test
  * <p>description:
+ * 配置OpenFeign的日志输出级别。需要配合客户端的配置才能生效，例如
+ * <pre>{@code
+ *    # 将对应包下的客户端的日志输出级别配置为debug
+ *    logging.level.[feign client所在的包]=debug
+ * }</pre>
  *
  * @author : consi
  * @since : 2022/7/12
--- taxi-common/src/main/java/com/example/config/LoadBalancerConfig.java
+++ taxi-common/src/main/java/com/example/config/LoadBalancerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 /**
  * project : taxi-test
  * <p>description:
+ * 配置负载均衡器的实现算法
  *
  * @author : consi
  * @since : 2022/7/12
